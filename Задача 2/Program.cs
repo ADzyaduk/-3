fa17f1fafd92a3f0adfd6cf77da4a005b87a80a6
Alex Dz <avdzyaduk@gmail.com>
2022-10-14 22:23:15 +0300

    Семинар 5

---
+++ Задача 2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void ChangeArray(int[] array)
+{    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+Console.Clear();
+int[] array = GetArray(6, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+ChangeArray();
+Console.WriteLine(String.Join(", ", array));
