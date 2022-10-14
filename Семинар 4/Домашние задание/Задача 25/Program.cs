d3970dbc7ecc614ce300d83ccfcec8706571e669
AlexanderD <avdzyaduk@gmail.com>
2022-10-14 11:31:22 +0300

    Задача 25

---
+++ Семинар 4/Домашние задание/Задача 25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+
+Console.Write ("Введите A: ");
+int a = int.Parse (Console.ReadLine ()!);
+Console.Write ("Введите B: ");
+int b = int.Parse (Console.ReadLine ()!);
+int mult = a;
+
+for (int i = 1; i < b; i ++) 
+    mult = mult*a;
+    Console.WriteLine($"Число {a} в степени {b} равно {multy}");
+
