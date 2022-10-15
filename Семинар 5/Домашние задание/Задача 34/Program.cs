1fa044bef9122e5632a04677921d777bcf6ae821
AlexanderD <avdzyaduk@gmail.com>
2022-10-15 18:15:09 +0300

    321

--- Семинар 5/Домашние задание/Задача 34/Program.cs
+++ Семинар 5/Домашние задание/Задача 34/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+Console.Clear();
+int[] array = GetArray(12, 100, 1000);
