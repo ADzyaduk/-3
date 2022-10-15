8fd4b293398452d42b6bac7da47b65b9683a6658
AlexanderD <avdzyaduk@gmail.com>
2022-10-15 17:35:17 +0300

    29

---
+++ Семинар 4/Домашние задание/Задача 29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Решение в группах задач:
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+        if (i != mass.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите массив через пробел: ");
+string input = Console.ReadLine();
+int[] ArrayResult = GetArrayFromString(input);
+PrintArray(ArrayResult);
