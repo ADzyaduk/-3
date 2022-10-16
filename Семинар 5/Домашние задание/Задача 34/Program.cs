c5b68ce7fbf37b250d6709366e4b3d779368ce15
AlexanderD <avdzyaduk@gmail.com>
2022-10-16 22:52:15 +0300

    34

--- Семинар 5/Домашние задание/Задача 34/Program.cs
+++ Семинар 5/Домашние задание/Задача 34/Program.cs
@@ -18,11 +18,11 @@ int Even(int[] array)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-            if (array[i] % 2 == 0)
-            {
-                count++;
-            }
+        if (array[i] % 2 == 0)
+        {
+            count++;
         }
+    }
     return count;
 }
 
