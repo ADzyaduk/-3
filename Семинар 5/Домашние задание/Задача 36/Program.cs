2b8ed4603ddf55b96a9ca78f4e00da470265209d
AlexanderD <avdzyaduk@gmail.com>
2022-10-16 23:42:33 +0300

    Задача 36

---
+++ Семинар 5/Домашние задание/Задача 36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int ElSum(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    return sum;
+}
+
+Console.Clear();
+int[] array = GetArray(10, 1, 100);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine();
+Console.Write($"Сумма чисел на четеных индексах = {ElSum(array)}");
