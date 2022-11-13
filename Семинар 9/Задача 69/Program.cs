7ddb7cb20ceb7ebbf00ae496719cd347cd914b71
Alex Dz <avdzyaduk@gmail.com>
2022-11-13 22:01:14 +0300

    s9 dz

---
+++ Семинар 9/Задача 69/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int PoW(int x, int y)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    else if (y == 1)
+    {
+        return x;
+    }
+    return x * PoW(x, y - 1);
+}
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень В: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PoW(a, b));
